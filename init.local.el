5aef2feac560f968c7f8c26b6161200837b6e3e4
Rongsong Shen <shenrs@cn.ibm.com>
2019-04-09 18:00:57 +0800

    rainbow delimiters color settings

--- init.local.el
+++ init.local.el
@@ -12,3 +12,16 @@
             ;; We remove Which Function Mode from the mode line, because it's mostly
             ;; invisible here anyway.
             (assq-delete-all 'which-func-mode mode-line-misc-info))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "dark orange"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "deep pink"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "chartreuse"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "deep sky blue"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "yellow"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "orchid"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "spring green"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "sienna1")))))
