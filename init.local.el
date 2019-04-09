824fce5c6bac0846aa886a0785c2041a471511bf
Rongsong Shen <shenrs@cn.ibm.com>
2019-04-09 10:52:59 +0800

    add setting of which function mode

--- init.local.el
+++ init.local.el
@@ -4,3 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Inconsolata LGC" :foundry "nil" :slant normal :weight normal :height 141 :width normal)))))
+
+;;   setting for which function mode
+(setq-default header-line-format
+              '((which-func-mode ("" which-func-format " "))))
+(setq mode-line-misc-info
+            ;; We remove Which Function Mode from the mode line, because it's mostly
+            ;; invisible here anyway.
+            (assq-delete-all 'which-func-mode mode-line-misc-info))
