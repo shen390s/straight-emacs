ae69b83c90a65072708106ec4dc410362605bc0d
Rongsong Shen <rshen@shenrs.eu>
2019-04-13 22:31:10 +0800

    doom emacs using straight.el

--- core/core.el
+++ core/core.el
@@ -1,2 +1,10 @@
+
+(defvar doom-emacs-dir (file-truename user-emacs-directory))
+(defvar doom-core-dir (concat doom-emacs-dir "core/"))
+(defvar doom-modules-dir (concat doom-emacs-dir "modules/"))
+(defvar doom-local-dir (concat doom-emacs-dir ".local/"))
+
+(load (concat doom-core-dir "core-packages") nil t)
+
 (provide 'core)
 ;;; core.el ends here
